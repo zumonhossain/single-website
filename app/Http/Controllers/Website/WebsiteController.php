19d897c9411a7ff9aeccf202747e39135bab328d
zumonhossain <zumon15-13169@diu.edu.bd>
2022-07-26 12:04:20 +0600

    Backend Portfolio Daynamic

--- app/Http/Controllers/Website/WebsiteController.php
+++ app/Http/Controllers/Website/WebsiteController.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\HomeSlider;
 use App\Models\About;
+use App\Models\Portfolio;
 
 class WebsiteController extends Controller{
     public function index(){
+        $homePortfolio = Portfolio::latest()->get();
         $homeAbout = About::where('about_id',1)->firstOrFail();
         $homeSlider = HomeSlider::where('home_slider_id',1)->firstOrFail();
-        return view('website.home-all.index',compact('homeSlider','homeAbout'));
+        return view('website.home-all.index',compact('homeSlider','homeAbout','homePortfolio'));
     }
 
     public function homeAbout(){
